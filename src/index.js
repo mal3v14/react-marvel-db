14f7f0d62d5f979b3292dc34f2b9d26ef1de8dc4
Malik <malikkasumov2@gmail.com>
2025-07-25 01:00:31 +0300

    Add random character from DB to home page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
-import App from "./components/app/App";
+import App from "./components/app/App.js";
 import "./style/style.scss";
+
 
 const container = document.getElementById("root");
 const root = createRoot(container);
