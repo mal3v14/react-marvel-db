8b3e66426f707f793cc1e0a7ae2a5857022e8e7a
Malik <malikkasumov2@gmail.com>
2025-07-29 16:09:44 +0300

    feat: propTypeslib added, used for CharList and CharInfo

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -108,4 +110,7 @@ class CharList extends Component {
     }
 }
 
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired
+}
 export default CharList;
